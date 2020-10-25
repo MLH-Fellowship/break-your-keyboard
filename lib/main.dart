5c874168f35cce92ed3b55d4ecca45308f880676
Avarayr <mikamika83@gmail.com>
2020-10-24 20:30:03 -0700

    added a splashscreen & firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'core/dependency_injection/setup_locator.dart';
 import 'core/navigation/route_generator.dart';
 import 'core/service/context/context_provider_i.dart';
+import 'module/game/splash_screen/splash_screen.dart';
 import 'module/home/home_page.dart';
 
 void main() {
@@ -15,22 +17,32 @@ void main() {
 
 // ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        StreamProvider<User>.value(
-          value: FirebaseAuth.instance.authStateChanges(),
-        ),
-      ],
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Break Your Keyboard',
-        theme: ThemeData.dark(),
-        initialRoute: HomePage.route,
-        navigatorKey: sl<ContextProviderI>().getNavigationKey(),
-        onGenerateRoute: RouteGenerator.generateRoute,
-      ),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MultiProvider(
+            providers: [
+              StreamProvider<User>.value(
+                value: FirebaseAuth.instance.authStateChanges(),
+              ),
+            ],
+            child: MaterialApp(
+              debugShowCheckedModeBanner: false,
+              title: 'Break Your Keyboard',
+              theme: ThemeData.dark(),
+              initialRoute: HomePage.route,
+              navigatorKey: sl<ContextProviderI>().getNavigationKey(),
+              onGenerateRoute: RouteGenerator.generateRoute,
+            ),
+          );
+        }
+
+        return const SplashScreen();
+      },
     );
   }
 }
