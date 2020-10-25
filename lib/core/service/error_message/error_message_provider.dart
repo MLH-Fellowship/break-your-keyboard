20e1079f07a7c1b60e668ae5c7f143a84cfd3745
12827629 <michael.k.malak@gmail.com>
2020-10-25 18:49:52 +0200

    fix(toast): toast alignment for mobile

--- lib/core/service/error_message/error_message_provider.dart
+++ lib/core/service/error_message/error_message_provider.dart
@@ -8,7 +8,7 @@ class ErrorMessageProvider implements ErrorMessageProviderI {
     Fluttertoast.showToast(
         msg: message,
         toastLength: Toast.LENGTH_SHORT,
-        gravity: ToastGravity.CENTER,
+        gravity: ToastGravity.BOTTOM,
         backgroundColor: Colors.red,
         textColor: Colors.white,
         fontSize: 16.0);
